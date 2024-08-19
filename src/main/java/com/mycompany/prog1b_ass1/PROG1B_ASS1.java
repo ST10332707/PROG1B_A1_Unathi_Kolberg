0793bc8579d1faa813bdffd205cc28124f32d5a2
Unathi <Unathikolberg17@gmail.com>
2024-08-19 16:10:21 +0200

    New project

---
+++ src/main/java/com/mycompany/prog1b_ass1/PROG1B_ASS1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog1b_ass1;
+
+/**
+ *
+ * @author Unath
+ */
+public class PROG1B_ASS1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
